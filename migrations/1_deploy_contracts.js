0beaaaa9f0b8c4381d30c45cfaa3e62b9ed19b94
Negaex <gh@negaex.com>
2019-02-25 16:45:32 +1100

    Updated migration script

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -5,6 +5,18 @@ const EthSwap = artifacts.require("EthSwap");
 const ERC20Swap = artifacts.require("ERC20Swap");
 const ERC20WithFeesSwap = artifacts.require("ERC20WithFeesSwap");
 
+let ETHSwapAddress;
+let WBTCSwapAddress;
+let RENSwapAddress;
+let TUSDSwapAddress;
+let OMGSwapAddress;
+let ZRXSwapAddress;
+let DGXSwapAddress;
+let USDCSwapAddress;
+let GUSDSwapAddress;
+let DAISwapAddress;
+let PAXSwapAddress;
+
 module.exports = async function (deployer, network, _accounts) {
     const config = require("./config.js")(network);
 
@@ -12,29 +24,79 @@ module.exports = async function (deployer, network, _accounts) {
         return;
     }
 
-    // const ETHSwap = await deployer.deploy(EthSwap, config.VERSION);
-    const WBTCSwap = await deployer.deploy(ERC20Swap, config.VERSION, config.TOKENS.WBTC);
-    const RENSwap = await deployer.deploy(ERC20Swap, config.VERSION, config.TOKENS.REN);
-    const TUSDSwap = await deployer.deploy(ERC20Swap, config.VERSION, config.TOKENS.TUSD);
-    const OMGSwap = await deployer.deploy(ERC20Swap, config.VERSION, config.TOKENS.OMG);
-    const ZRXSwap = await deployer.deploy(ERC20Swap, config.VERSION, config.TOKENS.ZRX);
-    const DGXSwap = await deployer.deploy(ERC20WithFeesSwap, config.VERSION, config.TOKENS.DGX);
-    const USDCSwap = await deployer.deploy(ERC20Swap, config.VERSION, config.TOKENS.USDC);
-    const GUSDSwap = await deployer.deploy(ERC20Swap, config.VERSION, config.TOKENS.GUSD);
-    const DAISwap = await deployer.deploy(ERC20Swap, config.VERSION, config.TOKENS.DAI);
-    const PAXSwap = await deployer.deploy(ERC20Swap, config.VERSION, config.TOKENS.PAX);
+    await deployer.deploy(EthSwap, config.VERSION);
+    ETHSwapAddress = EthSwap.address;
 
-    console.log(JSON.stringify({
-        // ETHSwap: ETHSwap ? ETHSwap.address : "",
-        WBTCSwap: WBTCSwap ? WBTCSwap.address : "",
-        RENSwap: RENSwap ? RENSwap.address : "",
-        TUSDSwap: TUSDSwap ? TUSDSwap.address : "",
-        OMGSwap: OMGSwap ? OMGSwap.address : "",
-        ZRXSwap: ZRXSwap ? ZRXSwap.address : "",
-        DGXSwap: DGXSwap ? DGXSwap.address : "",
-        USDCSwap: USDCSwap ? USDCSwap.address : "",
-        GUSDSwap: GUSDSwap ? GUSDSwap.address : "",
-        DAISwap: DAISwap ? DAISwap.address : "",
-        PAXSwap: PAXSwap ? PAXSwap.address : "",
-    }, undefined, "    "))
+    const WBTCSwap = await deployer.deploy(ERC20Swap, config.VERSION, config.TOKENS.WBTC);
+    WBTCSwapAddress = WBTCSwap ? WBTCSwap.address : "";
+
+    const RENSwap = await deployer.deploy(ERC20Swap, config.VERSION, config.TOKENS.REN);
+    RENSwapAddress = RENSwap ? RENSwap.address : "";
+
+    const TUSDSwap = await deployer.deploy(ERC20Swap, config.VERSION, config.TOKENS.TUSD);
+    TUSDSwapAddress = TUSDSwap ? TUSDSwap.address : "";
+
+    const OMGSwap = await deployer.deploy(ERC20Swap, config.VERSION, config.TOKENS.OMG);
+    OMGSwapAddress = OMGSwap ? OMGSwap.address : "";
+
+    const ZRXSwap = await deployer.deploy(ERC20Swap, config.VERSION, config.TOKENS.ZRX);
+    ZRXSwapAddress = ZRXSwap ? ZRXSwap.address : "";
+
+    const DGXSwap = await deployer.deploy(ERC20WithFeesSwap, config.VERSION, config.TOKENS.DGX);
+    DGXSwapAddress = DGXSwap ? DGXSwap.address : "";
+
+    const USDCSwap = await deployer.deploy(ERC20Swap, config.VERSION, config.TOKENS.USDC);
+    USDCSwapAddress = USDCSwap ? USDCSwap.address : "";
+
+    const GUSDSwap = await deployer.deploy(ERC20Swap, config.VERSION, config.TOKENS.GUSD);
+    GUSDSwapAddress = GUSDSwap ? GUSDSwap.address : "";
+
+    const DAISwap = await deployer.deploy(ERC20Swap, config.VERSION, config.TOKENS.DAI);
+    DAISwapAddress = DAISwap ? DAISwap.address : "";
+
+    const PAXSwap = await deployer.deploy(ERC20Swap, config.VERSION, config.TOKENS.PAX);
+    PAXSwapAddress = PAXSwap ? PAXSwap.address : "";
 }
+
+function exitHandler(options, exitCode) {
+    if (options.cleanup) {
+        console.log(JSON.stringify({
+            ETHSwap: ETHSwapAddress,
+            WBTCSwap: WBTCSwapAddress,
+            RENSwap: RENSwapAddress,
+            TUSDSwap: TUSDSwapAddress,
+            OMGSwap: OMGSwapAddress,
+            ZRXSwap: ZRXSwapAddress,
+            DGXSwap: DGXSwapAddress,
+            USDCSwap: USDCSwapAddress,
+            GUSDSwap: GUSDSwapAddress,
+            DAISwap: DAISwapAddress,
+            PAXSwap: PAXSwapAddress,
+        }, undefined, "    "))
+    }
+
+    if (options.exit) process.exit();
+}
+
+//do something when app is closing
+process.on('exit', exitHandler.bind(null, {
+    cleanup: true,
+}));
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {
+    exit: true,
+}));
+
+// catches "kill pid" (for example: nodemon restart)
+process.on('SIGUSR1', exitHandler.bind(null, {
+    exit: true,
+}));
+process.on('SIGUSR2', exitHandler.bind(null, {
+    exit: true,
+}));
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {
+    exit: true,
+}));
--- migrations/config.js
+++ migrations/config.js
@@ -1,4 +1,22 @@
 module.exports = (network) => {
+    if (network.match(/mainnet/)) {
+        return {
+            VERSION: "1.0.1",
+            TOKENS: {
+                DGX: "0x4f3AfEC4E5a3F2A6a1A411DEF7D7dFe50eE057bF",
+                TUSD: "0x0000000000085d4780B73119b644AE5ecd22b376",
+                REN: "0x408e41876cccdc0f92210600ef50372656052a38",
+                WBTC: "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+                ZRX: "0xE41d2489571d322189246DaFA5ebDe1F4699F498",
+                OMG: "0xd26114cd6EE289AccF82350c8d8487fedB8A0C07",
+                USDC: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+                GUSD: "0x056fd409e1d7a124bd7017459dfea2f387b6d5cd",
+                DAI: "0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359",
+                PAX: "0x8e870d67f660d95d5be530380d0ec0bd388289e1",
+            }
+        };
+    }
+
     if (network.match(/development/) || network.match(/testnet/)) {
         return {
             VERSION: "1.0.1",
@@ -13,24 +31,6 @@ module.exports = (network) => {
                 TUSD: "0x525389752ffe6487d33EF53FBcD4E5D3AD7937a0",
                 DGX: "0x7d6D31326b12B6CBd7f054231D47CbcD16082b71",
                 PAX: "0x3584087444dabf2e0d29284766142ac5c3a9a2b7",
-            }
-        };
-    }
-
-    if (network.match(/mainnet/)) {
-        return {
-            VERSION: "1.0.1",
-            TOKENS: {
-                DGX: "0x4f3AfEC4E5a3F2A6a1A411DEF7D7dFe50eE057bF",
-                TUSD: "0x8dd5fbCe2F6a956C3022bA3663759011Dd51e73E",
-                REN: "0x21C482f153D0317fe85C60bE1F7fa079019fcEbD",
-                WBTC: "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
-                ZRX: "0xE41d2489571d322189246DaFA5ebDe1F4699F498",
-                OMG: "0xd26114cd6EE289AccF82350c8d8487fedB8A0C07",
-                USDC: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
-                GUSD: "0x056fd409e1d7a124bd7017459dfea2f387b6d5cd",
-                DAI: "0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359",
-                PAX: "0x8e870d67f660d95d5be530380d0ec0bd388289e1",
             }
         };
     }
